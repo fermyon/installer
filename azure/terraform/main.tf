7c70dd2f6f92ad866cf5ad275e57713ac6eb91cf
jldeen <jessicadeen@me.com>
2022-06-29 17:07:05 -0700

    + azure support WIP
    Signed-off-by: jldeen <jessicadeen@me.com>

---
+++ azure/terraform/main.tf
@@ -0,0 +1,220 @@
+# -----------------------------------------------------------------------------
+# Hashistack + Fermyon Platform versions
+# -----------------------------------------------------------------------------
+
+locals {
+  nomad_version    = "1.3.1"
+  nomad_checksum   = "d16dcea9fdfab3846e749307e117e33a07f0d8678cf28cc088637055e34e5b37"
+
+  consul_version   = "1.12.1"
+  consul_checksum  = "8d138267701fc3502dc6b01beb08ae8fac969022ab867f61bc945af38686ecc3"
+
+  vault_version    = "1.10.3"
+  vault_checksum   = "c99aeefd30dbeb406bfbd7c80171242860747b3bf9fa377e7a9ec38531727f31"
+
+  traefik_version  = "v2.7.0"
+  traefik_checksum = "348e444c390156a3d17613e421ec80e23874e2388ef0cc22d7ad00a5b9c7f21a"
+
+  bindle_version   = "v0.8.0"
+  bindle_checksum  = "26f68ab5a03c7e6f0c8b83fb199ca77244c834f25247b9a62312eb7a89dba93c"
+
+  spin_version     = "v0.2.0"
+  spin_checksum    = "f5c25a7f754ef46dfc4b2361d6f34d40564768a60d7bc0d183dc26fe1bdcfae0"
+
+  hippo_version    = "v0.17.0"
+  hippo_checksum   = "2a9690cd8546108fbd27a9f0c4898d1c2c171a76219803290b526e40da1c3211"
+
+  common_tags = {
+    FermyonInstallation = "localtest"
+  }
+}
+
+# -----------------------------------------------------------------------------
+# Azure Resource Group Defaults
+# -----------------------------------------------------------------------------
+resource "random_pet" "rg-name" {
+    prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = random_pet.rg-name.id
+    location = var.resource_group_location
+}
+
+# -----------------------------------------------------------------------------
+# Azure VNET, Subnet, IP
+# -----------------------------------------------------------------------------
+resource "azurerm_virtual_network" "default" {
+    name = "default"
+    address_space = ["10.0.0.0/16"]
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "defaultsubnet" {
+    name = "defaultsubnet"
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.default.name
+    address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "defaultIp" {
+    name = "defaultIP"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    allocation_method = "Static"
+}
+
+# -----------------------------------------------------------------------------
+# Azure NSG
+# -----------------------------------------------------------------------------
+resource "azurerm_network_security_group" "defaultnsg" {
+    name = "defaultNSG"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name = "SSH"
+        priority = 1001
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+
+# -----------------------------------------------------------------------------
+# Azure Network Interface
+# -----------------------------------------------------------------------------
+resource "azurerm_network_interface" "defaultnic" {
+    name = "defaultnic"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+        name                          = "defaultNicConfig"
+        subnet_id                     = azurerm_subnet.defaultsubnet.id
+        private_ip_address_allocation = "Static"
+        public_ip_address_id          = azurerm_public_ip.defaultIp.id
+  }
+}
+
+# Connect SG to NIC
+resource "azurerm_network_interface_security_group_association" "defaultnicsg" {
+    network_interface_id = azurerm_network_interface.defaultnic.id
+    network_security_group_id = azurerm_network_security_group.defaultnsg.id
+}
+
+# -----------------------------------------------------------------------------
+# Azure Storage
+# -----------------------------------------------------------------------------
+resource "random_id" "randomID" {
+    keepers = {
+        # Generate new ID only when new resource group is defined
+        resource_group = azurerm_resource_group.rg.name
+    }
+
+    byte_length = 8
+}
+
+resource "azurerm_storage_account" "defaultstorage" {
+    name = "diag${random_id.randomID.hex}"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+
+# -----------------------------------------------------------------------------
+# SSH Key
+# -----------------------------------------------------------------------------
+resource "tls_private_key" "ssh" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
+
+# -----------------------------------------------------------------------------
+# Azure VM
+# -----------------------------------------------------------------------------
+resource "azurerm_linux_virtual_machine" "defaultVM" {
+    name = "defaultVM"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    network_interface_ids = [azurerm_network_interface.defaultnic.id]
+    size = "Standard_DS1_v2"
+
+    os_disk {
+      name = "myFermyonDisk"
+      caching = "ReadWrite"
+      storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+      publisher = "Canonical"
+      offer = "UbuntuServer"
+      sku = "18.04-LTS"
+      version = "latest"
+    }
+
+    computer_name = "myfermyonvm"
+    admin_username = "fermyon"
+    disable_password_authentication = true
+
+    admin_ssh_key {
+      username = "fermyon"
+      public_key = tls_private_key.ssh.private_key_openssh
+    }
+
+    boot_diagnostics {
+      storage_account_uri = azurerm_storage_account.defaultstorage.primary_blob_endpoint
+    }
+
+
+    user_data = templatefile("${path.module}/scripts/user-data.sh",
+    {
+      dns_zone                = var.dns_host == "sslip.io" ? "${azurerm_public_ip.defaultIp.fqdn}.${var.dns_host}" : var.dns_host,
+      enable_letsencrypt      = var.enable_letsencrypt,
+
+      nomad_version           = local.nomad_version,
+      nomad_checksum          = local.nomad_checksum,
+
+      consul_version          = local.consul_version,
+      consul_checksum         = local.consul_checksum,
+
+      vault_version           = local.vault_version,
+      vault_checksum          = local.vault_checksum,
+
+      traefik_version         = local.traefik_version,
+      traefik_checksum        = local.traefik_checksum,
+
+      bindle_version          = local.bindle_version,
+      bindle_checksum         = local.bindle_checksum,
+
+      spin_version            = local.spin_version,
+      spin_checksum           = local.spin_checksum,
+
+      hippo_version           = local.hippo_version,
+      hippo_checksum          = local.hippo_checksum,
+      hippo_registration_mode = var.hippo_registration_mode
+      hippo_admin_username    = var.hippo_admin_username
+      # TODO: ideally, Hippo will support ingestion of the admin password via
+      # its hash (eg bcrypt, which Traefik and Bindle both support) - then we can remove
+      # the need to pass the raw value downstream to the scripts, Nomad job, ecc.
+      hippo_admin_password    = random_password.hippo_admin_password.result,
+    }
+  )
+}
+
+# -----------------------------------------------------------------------------
+# Hippo admin password
+# -----------------------------------------------------------------------------
+
+resource "random_password" "hippo_admin_password" {
+  length           = 22
+  special          = true
+  override_special = "!#%&*-_=+<>:?"
+}
---
+++ azure/terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_version = ">= 1.0.0"
+
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~>2.0"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+
+    subscription_id = "dfb5d696-98d8-447c-a14d-56f131f3c4a5"
+    tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+    client_id = "8ea5e1e0-a64f-4073-842f-07156f7b57b6"
+    client_secret = "BhGtcElLrC0lmGCy.UmazKs.1DV-1zZC13" 
+}
