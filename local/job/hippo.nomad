vdice/fermyon-installer

update local installer nomad configuration for Hippo to allow setting initial user and password

local/job/hippo.nomad
@@ -26,6 +26,29 @@ variable "os" {
   }
 }
 
+variable "registration_mode" {
+  type    = string
+  default = "AdministratorOnly"
+  description = "The Hippo registration mode. Options are 'Open', 'Closed' and 'AdministratorOnly'. (Default: AdministratorOnly)"
+
+  validation {
+    condition     = var.registration_mode == "Open" || var.registration_mode == "Closed" || var.registration_mode == "AdministratorOnly"
+    error_message = "The Hippo registration mode must be 'Open', 'Closed' or 'AdministratorOnly'."
+  }
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Username for the admin account"
+  default     = null
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password for the admin account"
+  default     = null
+}
+
 job "hippo" {
   datacenters = ["dc1"]
   type        = "service"
@@ -83,6 +106,11 @@ job "hippo" {
         Hippo__PlatformDomain = var.domain
         Scheduler__Driver     = "nomad"
         Nomad__Driver         = "raw_exec"
+        
+        # Registration configuration
+        Hippo__RegistrationMode            = var.registration_mode
+        Hippo__Administrators__0__Username = var.registration_mode == "AdministratorOnly" ? var.admin_username : ""
+        Hippo__Administrators__0__Password = var.registration_mode == "AdministratorOnly" ? var.admin_password : ""
 
         Database__Driver            = "sqlite"
         ConnectionStrings__Database = "Data Source=hippo.db;Cache=Shared"

fermyon/installer

Signed-off-by: Steven Murawski <stmuraws@microsoft.com>

local/job/hippo.nomad
@@ -26,6 +26,29 @@ variable "os" {
   }
 }
 
+variable "registration_mode" {
+  type    = string
+  default = "Open"
+  description = "The Hippo registration mode. Options are 'Open', 'Closed' and 'AdministratorOnly'. (Default: AdministratorOnly)"
+
+  validation {
+    condition     = var.registration_mode == "Open" || var.registration_mode == "Closed" || var.registration_mode == "AdministratorOnly"
+    error_message = "The Hippo registration mode must be 'Open', 'Closed' or 'AdministratorOnly'."
+  }
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Username for the admin account"
+  default     = null
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password for the admin account"
+  default     = null
+}
+
 job "hippo" {
   datacenters = ["dc1"]
   type        = "service"
@@ -83,6 +106,11 @@ job "hippo" {
         Hippo__PlatformDomain = var.domain
         Scheduler__Driver     = "nomad"
         Nomad__Driver         = "raw_exec"
+        
+        # Registration configuration
+        Hippo__RegistrationMode            = var.registration_mode
+        Hippo__Administrators__0__Username = var.registration_mode == "AdministratorOnly" ? var.admin_username : ""
+        Hippo__Administrators__0__Password = var.registration_mode == "AdministratorOnly" ? var.admin_password : ""
 
         Database__Driver            = "sqlite"
         ConnectionStrings__Database = "Data Source=hippo.db;Cache=Shared"